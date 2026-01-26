98f7b7a925d63846f4e6b2a46c989e384f254261
Simran02 <nagpuresimran02@gmail.com>
2026-01-26 16:48:14 +0530

    Add all files

---
+++ loop/pattern/first.java
@@ -0,0 +1,39 @@
+package loop.pattern;
+import java.util.*;
+public class first{
+    public static void main(String[] args){
+       int n=4;
+       for(int i=1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+            System.out.print("* ");
+         }
+
+         int spaces=2*(n-i);
+         for(int j=1;j<=spaces;j++){
+            System.out.print("  ");
+         }
+
+         for(int k=1;k<=i;k++){
+            System.out.print("* ");
+         }
+        System.out.println();
+       }
+
+        for(int i=n;i>=1;i--){
+         for(int j=1;j<=i;j++){
+            System.out.print("* ");
+         }
+
+         int spaces=2*(n-i);
+         for(int j=1;j<=spaces;j++){
+            System.out.print("  ");
+         }
+
+         for(int k=1;k<=i;k++){
+            System.out.print("* ");
+         }
+         System.out.println();
+       }
+    
+    }
+}
